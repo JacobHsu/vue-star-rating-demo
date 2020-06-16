4bc17739ee62b34c8beba103c814b995439438f2
jacobhsu <jacob.hsu.tw@gmail.com>
2020-06-16 15:14:01 +0800

    async/await axios

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
       @rating-selected="setRating"
     ></star-rating>
     <hr>
-    <div v-html="rating"></div>
+    <div v-html="ratingMsg"></div>
     <hr>
     先前評論總數: {{ lastNonce }} 
     <br>Transaction Details : <a :href="address" title="address" target="_blank">{{ address }}</a>
@@ -31,32 +31,59 @@ export default {
   },
   data() {
     return {
-      rating: "No Rating Selected",
+      ratingMsg: "No Rating Selected",
       currentRating: "No Rating",
       currentSelectedRating: "No Current Rating",
       boundRating: 3,
       lastNonce: 0,
       newNonce: 0,
       address:'',
+      showGetDataApi:''
     };
   },
   methods: {
-    getProduct() {
+    async setRatingAction(rating, getDetaCb) {
       const api =
         "https://tg-wallet-node.azurewebsites.net/poa/api/rating/product";
-      axios
+      const product = await axios
         .get(api, {
           params: {
             hash: '0x126e03e48bc4758d0df0cba481192d437abec3e550907f7f589a457016c5cdc0'
           }
         })
-        .then(this.handleGetProductSucc);
-    },
-    async postData(rating, newNonce, getDeta) {
-      
-      this.rating = '';
-      this.address = ''
+      const newNonce = product.data.data.nonce
 
+      this.ratingMsg = "You have Selected: " + rating + " stars 請等待內容..."+ newNonce;
+      this.lastNonce = newNonce - 1
+
+      await this.postData(rating, newNonce)
+
+      const delay = (interval) => {
+          return new Promise((resolve) => {
+              setTimeout(resolve, interval);
+          });
+      };
+
+      await delay(3000);
+
+      await getDetaCb(newNonce);     
+    },
+
+    async getProduct() {
+      const api =
+        "https://tg-wallet-node.azurewebsites.net/poa/api/rating/product";
+      const product = await axios
+        .get(api, {
+          params: {
+            hash: '0x126e03e48bc4758d0df0cba481192d437abec3e550907f7f589a457016c5cdc0'
+          }
+        })
+
+      const newNonce = product.data.data.nonce
+      this.lastNonce = newNonce - 1
+    },
+    async postData(rating, newNonce) {
+      
       const postApi = 'https://tg-wallet-node.azurewebsites.net/poa/api/rating/comments'
       let res = await axios.post(postApi, {
         "userAddress":"0x500F5C0c90B1301777c30d2576eAF2cf53845606",
@@ -68,32 +95,25 @@ export default {
       if(res.data)  {
         let transactionsAddr = res.data.data
         this.address = 'http://192.168.51.218/transactions/'+transactionsAddr+'/function'
-        setTimeout(function() {
-          getDeta();
-        }, 2000);
       }
     },
 
-    getDeta() {
-
+    getDeta(newNonce) {
+      const getDataApi = "https://tg-wallet-node.azurewebsites.net/poa/api/rating/comments?hash=0x126e03e48bc4758d0df0cba481192d437abec3e550907f7f589a457016c5cdc0&nonce="+newNonce
+      this.showGetDataApi = getDataApi
       const api =
         "https://tg-wallet-node.azurewebsites.net/poa/api/rating/comments";
       axios
         .get(api, {
           params: {
             hash: '0x126e03e48bc4758d0df0cba481192d437abec3e550907f7f589a457016c5cdc0',
-            nonce: this.newNonce 
+            nonce: newNonce 
           }
         })
       .then( this.handleGetDataSucc );
     },
-    handleGetProductSucc(res) {
-      const nonce = res.data.data.nonce
-      this.lastNonce = nonce -1
-      this.newNonce = res.data.data.nonce
-    },
     handleGetDataSucc(res) {
-
+      console.log('handleGetDataSucc',res, this.newNonce)
       res = res.data
       const userAddress = res.data.userAddress
       const star = res.data.star / 10
@@ -101,14 +121,19 @@ export default {
 
       let msg = `UserID:${userAddress}  <br>\
               評論內容: ${comments} <br>\
-              結果:${star}  顆星<br>`;
+              結果:${star}  顆星<br>
+              ${this.showGetDataApi}<br> `;
 
-      this.rating = msg 
+      this.ratingMsg = msg 
+    },
+    reset() {
+      this.ratingMsg = ''
+      this.address = ''
+      this.lastNonce = ''
     },
     setRating: function(rating) {
-      
-      const resPost = this.postData(rating, this.newNonce, this.getDeta)
-      this.rating = "You have Selected: " + rating + " stars 請等待內容...";
+      this.reset()
+      this.setRatingAction(rating, this.getDeta)
     },
     showCurrentRating: function(rating) {
       this.currentRating =
